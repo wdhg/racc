ba7840c363d50dfd6f4462ef4d454f304c7ec4e6
wdhg <me@wdhg.me>
2023-04-17 14:14:58 +0100

    Use preprocessor to switch between testing and actual main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,16 @@
+#ifndef RACC_TEST
+
 #include <stdio.h>
 
-int main() {
-  printf("Hello world!\n");
-  return 0;
+int main(void) {
+	printf("Hello world!\n");
+	return 0;
 }
+
+#else /* ========== TESTS ========== */
+
+#include <ctest.h>
+
+int main(void) { return 0; }
+
+#endif
